9050dc8ec46c96253f085880a7b4deb486958a13
Kevin Tate <2kmtate@gmail.com>
2020-11-16 23:14:30 -0500

    images and initial look and feel

---
+++ script.js
@@ -0,0 +1,21 @@
+(function () {
+  document.addEventListener("DOMContentLoaded", function() {
+    var headerEl = document.querySelector('header');
+    var h1El = headerEl.querySelector('h1');
+    var navItems = headerEl.querySelectorAll('.navLink');
+
+    document.addEventListener("scroll", function() {
+      if (window.pageYOffset) {
+        headerEl.classList.add('bgc-primary');
+        h1El.classList.add('color-white');
+        navItems.forEach(function(el) { el.classList.add('color-white')})
+      } else {
+        headerEl.classList.remove('bgc-primary');
+        h1El.classList.remove('color-white');
+        navItems.forEach(function(el) { el.classList.remove('color-white')})
+      }
+    });
+
+    var scroll = new SmoothScroll('a[href*="#"]');
+  });
+})();

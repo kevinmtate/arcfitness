730beaa3e625de02f7fb35417d53dbca29c1d0b8
Kevin Tate <2kmtate@gmail.com>
2021-05-18 21:27:31 -0400

    rebrand part 2

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 (function () {
+  var smallScreen = 480;
+  var mediumScreen = 960;
+
   document.addEventListener("DOMContentLoaded", function() {
-    var headerEl = document.querySelector('header');
-    var h1El = headerEl.querySelector('h1');
-    var navItems = headerEl.querySelectorAll('.navLink');
+    var headerEl = document.querySelector("header");
+    var h1El = headerEl.querySelector("h1");
+    var navItems = headerEl.querySelectorAll(".navLink");
+    var hamburgerEl = headerEl.querySelector("#hamburger");
+    var headerContent = headerEl.querySelector(".headerContent");
 
     document.addEventListener("scroll", function() {
+      if (window.screen.width < mediumScreen) {
+        return;
+      }
+
       if (window.pageYOffset) {
-        headerEl.classList.add('bgc-secondary');
-        h1El.classList.add('color-white');
-        navItems.forEach(function(el) { el.classList.add('color-white')})
+        headerContent.classList.add("bgc-secondary");
+        h1El.classList.add("color-white");
+        navItems.forEach(function(el) { el.classList.add("color-white")})
       } else {
-        headerEl.classList.remove('bgc-secondary');
-        h1El.classList.remove('color-white');
-        navItems.forEach(function(el) { el.classList.remove('color-white')})
+        headerContent.classList.remove("bgc-secondary");
+        h1El.classList.remove("color-white");
+        navItems.forEach(function(el) { el.classList.remove("color-white")})
       }
     });
+    
+    hamburgerEl.addEventListener("click", function() {
+      headerContent.classList.toggle("headerContent-show");
+    });
 
-    var scroll = new SmoothScroll('a[href*="#"]');
+    navItems.forEach(function(el) {
+      el.addEventListener("click", function() {
+        if (window.screen.width < smallScreen) {
+          headerContent.classList.remove("headerContent-show");
+        }
+      });
+    })
+
+    var scroll = new SmoothScroll("a[href*='#']");
   });
 })();
